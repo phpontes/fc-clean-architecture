fe40410427633923bdd3edf3b3ce7a2b77259d58
Paulo Henrique Pontes <115827846+phpontes@users.noreply.github.com>
2025-08-25 14:20:54 +0000

    implementando testes e endpoint de listagem de Customers

--- src/infrastructure/api/__tests__/customer.e2e.spec.ts
+++ src/infrastructure/api/__tests__/customer.e2e.spec.ts
@@ -37,4 +37,42 @@ describe("Customer E2E tests", () => {
     });
     expect(response.status).toBe(500);
   });
+
+  it("should list all customer", async () => {
+    const response = await request(app)
+      .post("/customer")
+      .send({
+        name: "Jão",
+        address: {
+          street: "Street",
+          city: "City",
+          number: 123,
+          zip: "12345",
+        },
+      });
+    expect(response.status).toBe(200);
+    const response2 = await request(app)
+      .post("/customer")
+      .send({
+        name: "Jana",
+        address: {
+          street: "Street 2",
+          city: "City 2",
+          number: 1234,
+          zip: "12344",
+        },
+      });
+    expect(response2.status).toBe(200);
+
+    const listResponse = await request(app).get("/customer").send();
+
+    expect(listResponse.status).toBe(200);
+    expect(listResponse.body.customers.length).toBe(2);
+    const customer = listResponse.body.customers[0];
+    expect(customer.name).toBe("Jão");
+    expect(customer.address.street).toBe("Street");
+    const customer2 = listResponse.body.customers[1];
+    expect(customer2.name).toBe("Jana");
+    expect(customer2.address.street).toBe("Street 2");
+  });
 });
