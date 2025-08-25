1ede8355fd3b80bb937e8103e62aeaa1163f8932
Paulo Henrique Pontes <115827846+phpontes@users.noreply.github.com>
2025-08-25 12:58:30 +0000

    implementando listagem de Customer

--- src/usecase/customer/list/list.customer.unit.spec.ts
+++ src/usecase/customer/list/list.customer.unit.spec.ts
@@ -1,5 +1,6 @@
 import CustomerFactory from "../../../domain/customer/factory/customer.factory";
 import Address from "../../../domain/customer/value-object/address";
+import ListCustomerUseCase from "./list.customer.usecase";
 
 const customer1 = CustomerFactory.createWithAddress(
   "Jão Silva",
